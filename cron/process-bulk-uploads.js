9b32ef3efb3b38229dbf77e27384a2f686773d9c
Zaven <matthew.fullstackgis@gmail.com>
2020-07-03 19:46:50 -0700

    v0.1 data pipeline services

---
+++ cron/process-bulk-uploads.js
@@ -0,0 +1,110 @@
+const Config = require('./config/config');
+const http = require('http');
+const rp = require('request-promise-native');
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+  connectionString: Config.connectionString
+});
+
+//const Sentry = require('@sentry/node');
+//Sentry.init({ dsn: Config.sentryDSN });
+
+
+(async () => {
+
+  const query = {
+    text: `SELECT *
+    FROM bulk_tree_upload
+    WHERE processed = FALSE`
+  };
+  const rval = await pool.query(query);
+  console.log(rval.rows);
+
+  for(let row of rval.rows){
+    console.log(row);
+    const bulkData = row.bulk_data;
+    const requests = [];
+    if(bulkData.trees != null){
+      for(let tree of bulkData.trees){
+        console.log(tree);
+
+        var options = {
+          method: 'POST',
+          uri: Config.dataInputMicroserviceURI + "tree",
+          body: tree,
+          json: true // Automatically stringifies the body to JSON
+        };
+
+        const promise = rp(options);
+        requests.push(promise);
+      
+      }
+
+    }
+
+    if(bulkData.registrations != null){
+
+      for(let planter of bulkData.registrations){
+        console.log(planter);
+
+        var options = {
+          method: 'POST',
+          uri: Config.dataInputMicroserviceURI + "planter",
+          body: planter,
+          json: true // Automatically stringifies the body to JSON
+        };
+
+        const promise = rp(options);
+        requests.push(promise);
+      
+      }
+    }
+
+    if(bulkData.devices != null){
+
+      for(let device of bulkData.devices){
+        console.log(device);
+
+        var options = {
+          method: 'PUT',
+          uri: Config.dataInputMicroserviceURI + "device",
+          body: device,
+          json: true // Automatically stringifies the body to JSON
+        };
+
+        const promise = rp(options);
+        requests.push(promise);
+      
+      }
+
+    }
+
+
+    const result = await Promise.all(requests);
+    console.log(result);
+
+    const update = {
+      text: `UPDATE bulk_tree_upload
+      SET processed = TRUE,
+      processed_at = now()
+      WHERE id = $1`,
+      values: [row.id]
+    };
+    const rvalUpdate = await pool.query(update);
+    console.log(`Processed bulk tree upload ${row.id}`);
+
+  }
+
+  pool.end();
+  process.exit(0);
+
+})().catch(e => {
+
+  console.log(e);
+  //Sentry.captureException(e);
+  pool.end();
+
+  console.log('notify-slack-reports done with catch');
+  process.exit(1);
+})
---
+++ microservice/src/data.js
@@ -0,0 +1,206 @@
+class Data {
+
+  constructor(pool) {
+    this.pool = pool;
+  }
+
+  async checkForExistingTree(uuid) {
+    
+    const getQuery = {
+      text: `SELECT *
+      FROM trees
+      WHERE uuid = $1`,
+      values: [uuid]
+    }
+    const rval = await this.pool.query(getQuery)
+      .catch(err => {
+        console.log(`ERROR: FAILED TO GET CHECK UUID ON TREES ${err}`)
+        throw(err);
+      });
+    if(rval.rows.length > 0){
+      return rval.rows[0];
+    } else {
+      return null;
+    }
+  }
+
+  async createTree(planterId, deviceIdentifier, body) {
+    let lat = body.lat;
+    let lon = body.lon;
+    let gpsAccuracy = Math.floor(body.gps_accuracy);
+    let timestamp = body.timestamp;
+    let imageUrl = body.image_url; // first image
+    let planterPhotoUrl = body.planter_photo_url ? body.planter_photo_url : "";
+    let planterIdentifier = body.planter_identifier ? body.planter_identifier : "";
+    let note = body.note ? body.note : "";
+    let uuid = body.uuid; // This is required
+    let imagesJson = body.images ? body.images : {};
+    let domainSpecificDataJson = body.domain_specific_data ? body.domain_specific_data : {};
+
+    const geometry = 'POINT( ' + lon + ' ' + lat + ')';
+    const insertQuery = {
+      text: `INSERT INTO
+      trees(planter_id,
+        lat,
+        lon,
+        gps_accuracy,
+        time_created,
+        time_updated,
+        image_url,
+        estimated_geometric_location,
+        planter_photo_url,
+        planter_identifier,
+        device_identifier,
+        note,
+        uuid,
+        images,
+        domain_specific_data
+      )
+      VALUES($1, $2, $3, $4, to_timestamp($5), to_timestamp($5), $6, ST_PointFromText($7, 4326), $8, $9, $10, $11, $12, $13, $14 ) RETURNING *`,
+      values: [planterId, lat, lon, gpsAccuracy, timestamp, imageUrl, geometry, planterPhotoUrl, planterIdentifier, deviceIdentifier, note, uuid, JSON.stringify(imagesJson), JSON.stringify(domainSpecificDataJson) ],
+    }
+
+    const rval = await this.pool.query(insertQuery)
+      .catch(err => {
+        console.log(`ERROR: FAILED TO CREATE TREE ${err}`)
+        console.log(insertQuery);
+        throw(err);
+      });
+    const tree = rval.rows[0];
+
+    tree.attributes = await this.insertTreeAttributes(tree.id, body.attributes);
+
+    return tree;
+
+  }
+
+  async insertTreeAttributes(id, attributes){
+    var results = [];
+    if(attributes) {
+      for(let attribute of attributes){
+        const insert = {
+          text: `INSERT INTO tree_attributes
+          (tree_id, key, value)
+          values
+          ($1, $2, $3)
+          RETURNING *`,
+          values: [id, attribute.key, attribute.value]
+        }
+        const stored = await this.pool.query(insert);
+        results.push(stored.rows[0]);
+      }
+    }
+    return results;
+  }
+
+  async trees() {
+    let query = {
+      text: `SELECT *
+      FROM trees
+      WHERE active = true`
+    }
+    const rval = await this.pool.query(query);
+    return rval.rows;
+  }
+
+  async treesForUser(planterId){
+
+    let query = {
+      text: `SELECT *
+      FROM trees
+      WHERE planter_id = $1
+      AND active = true`,
+      values: [planterId]
+    }
+    const rval = await this.pool.query(query);
+    return rval.rows;
+
+  }
+
+  async createPlanterRegistration(planterId, deviceIdentifier, body){
+    var query = {
+      text: 'INSERT INTO planter_registrations ( planter_id, device_identifier, first_name, last_name, organization, phone, email, lat, lon ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *',
+      values: [planterId, deviceIdentifier, body.first_name, body.last_name, body.organization, body.phone, body.email, body.lat, body.lon]
+    }
+    const rval = await this.pool.query(query);
+    return rval.rows[0];
+  }
+
+  async findUser(identifier){
+    return await this.findOrCreateUser(identifier, "Anonymous", "Planter", "");
+  }
+
+  async findOrCreateUser(identifier, first_name, last_name, organization){
+      let query = {
+      text: 'SELECT * FROM planter WHERE phone = $1 OR email = $1',
+      values: [identifier]
+      }
+      const data = await this.pool.query(query);
+      if(data.rows.length == 0){
+          var reg = new RegExp('^\\d+$');
+          var query2 = null;
+          if(reg.test(identifier)){
+              query2 = {
+              text: 'INSERT INTO planter (first_name, last_name, organization, phone) VALUES ($1, $2, $3, $4 ) RETURNING *',
+              values: [first_name, last_name, organization, identifier]
+              }
+          } else {
+              query2 = {
+              text: 'INSERT INTO planter (first_name, last_name, organization, email) VALUES ($1, $2, $3, $4 ) RETURNING *',
+              values: [first_name, last_name, organization, identifier]
+              }
+          }
+          const rval = await this.pool.query(query2);
+          return rval.rows[0];
+
+      } else {
+        return data.rows[0]
+      }
+  }
+
+
+  async upsertDevice(body, callback){
+    let android_id = body['device_identifier']
+    let app_version = body['app_version']
+    let app_build = body['app_build']
+    let manufacturer = body['manufacturer']
+    let brand = body['brand']
+    let model = body['model']
+    let hardware = body['hardware']
+    let device = body['device']
+    let serial = body['serial']
+    let android_release = body['androidRelease']
+    let android_sdk = body['androidSdkVersion']
+
+    // insert only if one does not exist
+    const insert = {
+      text: `INSERT INTO devices (android_id) values ($1) ON CONFLICT (android_id) DO NOTHING`,
+      values: [android_id]
+    };
+    await this.pool.query(insert);
+
+    const query = {
+      text: `UPDATE devices
+      SET app_version = ($1),
+      app_build = ($2),
+      manufacturer = ($3),
+      brand = ($4),
+      model = ($5),
+      hardware = ($6),
+      device = ($7),
+      serial = ($8),
+      android_release = ($9),
+      android_sdk = ($10)
+      WHERE android_id = ($11)
+      RETURNING *`,
+      values: [app_version, app_build, manufacturer, brand, model, hardware, device, serial, android_release, android_sdk, android_id]
+    };
+    const returningDevice = await this.pool.query(query);
+    return returningDevice.rows[0];
+  }
+
+}
+
+
+
+module.exports = Data;
